dcce44564101cfa2824a50caeaae01c496aad713
Roby Hartono <robyhartonodev312@gmail.com>
2022-09-28 13:14:43 +0200

    Added assets icon, logo and splash screen for capacitor and web version

--- src/components/JourneyForm.vue
+++ src/components/JourneyForm.vue
@@ -1,13 +1,35 @@
 <template>
-  <div>My component</div>
+  <OnClickOutside @trigger="closeForm">
+    <div class="custom-wrapper bg-white shadow-4 q-pa-lg">
+      <div class="text-h5 text-primary q-mt-md">New Record</div>
+    </div>
+  </OnClickOutside>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import { OnClickOutside } from '@vueuse/components';
+
 export default defineComponent({
   name: 'JourneyForm',
-  setup() {
-    return {};
+  components: { OnClickOutside },
+  setup(props, { emit }) {
+    function closeForm() {
+      emit('close');
+    }
+
+    return {
+      closeForm,
+    };
   },
 });
 </script>
+
+<style scoped>
+  .custom-wrapper {
+    border-top-left-radius: 0.75rem; /* 6px */
+    border-top-right-radius: 0.75rem; /* 6px */
+    height: 80vh;
+    width: 100vw;
+  }
+</style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -3,20 +3,50 @@
     <journey-list></journey-list>
 
     <q-page-sticky position="bottom-right" :offset="[18, 18]">
-      <q-btn round color="secondary" icon="add_circle" size="lg" />
+      <q-btn
+        round
+        color="secondary"
+        icon="add_circle"
+        size="lg"
+        @click="openJourneyForm"
+      />
+    </q-page-sticky>
+
+    <q-page-sticky position="bottom" :offset="[0, 0]">
+      <transition
+        enter-active-class="animated slower slideInUp"
+        leave-active-class="animated slower slideOutDown"
+      >
+        <journey-form v-show="showJourneyForm" @close="onCloseJourneyForm()" />
+      </transition>
     </q-page-sticky>
   </q-page>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
 import JourneyList from 'src/components/JourneyList.vue';
+import JourneyForm from 'src/components/JourneyForm.vue';
 
 export default defineComponent({
   name: 'IndexPage',
-  components: { JourneyList },
+  components: { JourneyList, JourneyForm },
   setup() {
-    return {};
+    const showJourneyForm = ref(false);
+
+    function openJourneyForm() {
+      showJourneyForm.value = true;
+    }
+
+    function onCloseJourneyForm() {
+      showJourneyForm.value = false;
+    }
+
+    return {
+      showJourneyForm,
+      openJourneyForm,
+      onCloseJourneyForm,
+    };
   },
 });
 </script>
